1e3531edc1abd54530e1298f62a92fd10a79a41e
IsenfireLDC <klempb@msoe.edu>
2020-01-26 17:09:40 -0600

    Added new testing files to repo

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: Jan 26, 2020
+ *      Author: klempb
+ */
+
+#include "ParserTest.h"
+
+int main() {
+	return parserTest();
+}
---
+++ Test.h
@@ -0,0 +1,41 @@
+/*
+ * Test.h
+ *
+ *  Created on: Jan 26, 2020
+ *      Author: klempb
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+
+string TokenNames[] = {
+	"UnexpectedEnd",
+	"Null",
+	"EOL",
+	"Identifier",
+	"Number",
+	"EOI",
+	"Semicolon",
+	"Add",
+	"Sub",
+	"Mul",
+	"Div",
+	"Power",
+	"Equal",
+	"Greater",
+	"GreaterOrEqual",
+	"Less",
+	"LessOrEqual",
+	"Assign",
+	"AddAssign",
+	"SubAssign",
+	"Incr",
+	"Decr",
+	"LParan",
+	"RParan",
+	"Quote",
+};
+
+
+#endif /* TEST_H_ */
